0adc77fa8457c611dbe6f0bd296e0da3e80c3885
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2022-11-09 16:50:46 +0200

    Inductor doesn't like arbitrary functions on the graph

--- torch/_prims/nvfuser_executor.py
+++ torch/_prims/nvfuser_executor.py
@@ -409,12 +409,12 @@ def maybe_partition_graph(
         # nvprims but won't be sent to nvFuser with a call to PyTorch's eager
         # mode. This is necessary because torch.ops.* have higher overhead than
         # calling the eager mode directly.
-        for node in partitioned_graph.graph.nodes:
-            if node.op == "call_function" and str(node.target).startswith("nvprims."):
-                if getattr(node.target, "impl_aten", None) is not None:
-                    node.target = node.target.impl_aten
-        partitioned_graph.graph.eliminate_dead_code()
-        partitioned_graph.recompile()
+        # for node in partitioned_graph.graph.nodes:
+        #     if node.op == "call_function" and str(node.target).startswith("nvprims."):
+        #         if getattr(node.target, "impl_aten", None) is not None:
+        #             node.target = node.target.impl_aten
+        # partitioned_graph.graph.eliminate_dead_code()
+        # partitioned_graph.recompile()
         return partitioned_graph, any_unsupported
     else:
         return gm, any_unsupported
